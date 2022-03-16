d0b04a0cd02b0d41e0941ecaa26428578468dbce
playtime881 <lucas2@live.com.au>
2022-03-16 18:23:59 +1100

    mer similar export working

--- dbTools.py
+++ dbTools.py
@@ -1,5 +1,6 @@
+from json import dump as makeJSON
+
 def stringFromArray(array, start_index, char_count):
-	
 	return_string = ""
 
 	if (start_index + char_count > len(array) or start_index < 0):
@@ -32,16 +33,29 @@ def LEReadData(array, start_index, data_type):
 	
 	return return_num
 
-def getEntryDict(array, start_index, num_entries):
+'''def getEntryDict(array, start_index, num_entries):
 	index_dict = {}
 	
 	for i in range(num_entries):
 		index_short = LEReadData(array, start_index + (2 * i), "short")
+		if index_short == 0:
+			continue
 
-		if index_short != 65535:
+		if (index_short != 65535 and index_short not in index_dict):
 			index_dict[index_short] = i
 	
-	return index_dict
+	return index_dict'''
+
+
+def getEntryList(array, start_index, num_entries):
+	index_list = []
+	
+	for i in range(num_entries):
+		index_short = LEReadData(array, start_index + (2 * i), "short")
+		if (index_short == len(index_list)):
+			index_list.append(i)
+	
+	return index_list
 
 def makeEntry(array, start_index, instructions):
 	return_entry = []
@@ -86,11 +100,65 @@ def isInt(string):
 	except:
 		return False
 
-def makeDB(array, start_index, num_entries, entry_dict):
+def makeDB(array, start_index, song_indices):
 	return_list = []
-	entry_instructions = [64, 64, 64, 64, "i", "i", "i", "i", -8, "s", -6, "a5", "a5", -10, "i", -120, "i", -508, "id", -16, 16]
+	entry_instructions = [64, 64, 64, 64, "i", "i", "i", "i", -8, "s", -6, "a5", "a5", -10, "i", -120, "i", -508, -4, -16, 16]
 	
-	for i in range(num_entries):
-		return_list.append(makeEntry(array, start_index + 1324 * i, entry_instructions))
+	for i in range(len(song_indices)):
+		return_list.append([song_indices[i]] + makeEntry(array, start_index + 1324 * i, entry_instructions))
 
 	return return_list
+
+
+
+def makeSongData(songDataArray):
+	return_list = []
+
+	for i in range(len(songDataArray)):
+		new_dict = {
+			"song_id": songDataArray[i][0],
+            "title": songDataArray[i][1],
+            "title_ascii": songDataArray[i][2],
+            "genre": songDataArray[i][3],
+            "artist": songDataArray[i][4],
+            "texture_title": songDataArray[i][5],
+            "texture_artist": songDataArray[i][6],
+            "texture_genre": songDataArray[i][7],
+            "texture_load": songDataArray[i][8],
+            #"texture_list": songDataArray[i][4],
+            #"font_idx": 0,
+            "game_version": songDataArray[i][9],
+            #"other_folder": 1,          fskip 2
+            #"bemani_folder": 0,         fskip 2
+            #"splittable_diff": 0,       fskip 3
+            "difficulties": songDataArray[i][10] + songDataArray[i][11],
+            #"volume": 105,        fskip
+
+#            "file_identifiers": [              fskip 10
+#                48,
+#                49,
+#                97,
+#                97,
+#                48,
+#                48,
+#                50,
+#                97,
+#                97,
+#                48
+#            ],
+
+            "bga_filename": songDataArray[i][14]
+		}
+
+		return_list.append(new_dict)
+	return return_list
+
+
+def exportDB(songDataArray, header_array, output_path = "db.json"):
+	outputDict = {
+		"data_ver": header_array[1],
+		"data": makeSongData(songDataArray)
+	}
+
+	with open(output_path, "w", encoding = "utf-8") as json_path:
+		makeJSON(outputDict, json_path, indent = 4)
--- main.py
+++ main.py
@@ -9,33 +9,37 @@ with open(dbpath, "rb") as bin_file:
 
 bc = 0    #byte counter
 
+header_instructions = [4, "i", "s", "s"]      #magic_string, version, entry_number, index_number
+header_array = dbt.makeEntry(byte_array, bc, header_instructions)
 
-magic_string = dbt.stringFromArray(byte_array, bc, 4)    #first 4 bytes are magic string (1 character per byte)
-bc += 4
-
-version = dbt.LEReadData(byte_array, bc, "int")                  #next 4 bytes are game version (integer)
-bc += 4
-
-entry_count = dbt.LEReadData(byte_array, bc, "short")    #next 2 bytes are amount of songs in db
-bc += 2
-
-index_count = dbt.LEReadData(byte_array, bc, "short")    #next 2 bytes are total amount of indices available, (version + 1) * 1000 in this case
-bc += 2
-
-
-
-bc += 4              #skip 4 empty bytes
+bc += 16              #skip 4 empty bytes
 
 
 
 #coming up is index_count * 2 bytes (2 bytes for each index). The value of each index tells of the value in the entries 
 
-song_dict = dbt.getEntryDict(byte_array, bc, index_count)
-bc += index_count * 2
 
-music_db = dbt.makeDB(byte_array, bc, entry_count, song_dict)
-bc += 1324 * entry_count
 
+
+'''
+song_index_dict = dbt.getEntryDict(byte_array, bc, header_array[3])
+bc += header_array[3] * 2
+'''
+
+song_index_list = dbt.getEntryList(byte_array, bc, header_array[3])
+bc += header_array[3] * 2
+
+if len(song_index_list) != header_array[2]:
+	raise ValueError("Scores in array do not match number in header.\nEntries listed in header: " + str(header_array[2]) + "\nEntries successfully found: " + len(song_index_list))
+
+music_db = dbt.makeDB(byte_array, bc, song_index_list)
+bc += 1324 * len(song_index_list)
+
+
+dbt.exportDB(music_db, header_array)
+
+
+'''
 kkAlias = ["kors k", "Disconation", "StripE", "teranoid", "Eagle", "maras k", "The 4th"]
 
 for song in music_db:
@@ -47,10 +51,6 @@ for song in music_db:
 	
 	if (found == False):
 		continue
-
-	song[1] = "kors k's How to make " + song[3]
-
-	print(song[3])
-	print(song[1])
-	print(song[4] + "\n")
-
+	
+	print(song)
+'''
